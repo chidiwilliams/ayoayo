9bd81dd690a8f2d1edd0a97eee618180b9185765
Chidi Williams <williamschidi1@gmail.com>
2020-06-09 14:50:36 +0100

    Change test runner to tap

--- ayoayo/index.test.js
+++ ayoayo/index.test.js
@@ -1,41 +1,49 @@
-const assert = require('assert');
 const Ayoayo = require('.');
+const tap = require('tap');
 
-describe('Ayoayo', function () {
-  it('should have the correct board and captured values', function () {
-    const ayoayo = new Ayoayo();
-    ayoayo.play(0);
-    ayoayo.play(2);
-    ayoayo.play(4);
-    ayoayo.play(2);
-    ayoayo.play(0);
-    ayoayo.play(0);
-    ayoayo.play(2);
-    ayoayo.play(0);
-    ayoayo.play(5);
-    ayoayo.play(2);
-    ayoayo.play(5);
-    ayoayo.play(3);
-    ayoayo.play(4);
-    ayoayo.play(4);
-    ayoayo.play(2);
-    ayoayo.play(1);
-    ayoayo.play(0);
-    ayoayo.play(3);
-    ayoayo.play(5);
-    ayoayo.play(5);
-    ayoayo.play(5);
-    ayoayo.play(1);
-    ayoayo.play(3);
-    ayoayo.play(0);
-    ayoayo.play(1);
+tap.test('should finish with the correct board and captured values', function (
+  t,
+) {
+  const ayoayo = new Ayoayo();
+  ayoayo.play(0);
+  ayoayo.play(2);
+  ayoayo.play(4);
+  ayoayo.play(2);
+  ayoayo.play(0);
+  ayoayo.play(0);
+  ayoayo.play(2);
+  ayoayo.play(0);
+  ayoayo.play(5);
+  ayoayo.play(2);
+  ayoayo.play(5);
+  ayoayo.play(3);
+  ayoayo.play(4);
+  ayoayo.play(4);
+  ayoayo.play(2);
+  ayoayo.play(1);
+  ayoayo.play(0);
+  ayoayo.play(3);
+  ayoayo.play(5);
+  ayoayo.play(5);
+  ayoayo.play(5);
+  ayoayo.play(1);
+  ayoayo.play(3);
+  ayoayo.play(0);
+  ayoayo.play(1);
 
-    assert.deepEqual(ayoayo.board, [
-      [0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0],
-    ]);
-    assert.deepEqual(ayoayo.captured, [24, 24]);
-    assert.equal(ayoayo.isGameOver, true);
-    assert.equal(ayoayo.winner, -1);
-  });
+  t.deepEqual(ayoayo.board, [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+  ]);
+  t.deepEqual(ayoayo.captured, [24, 24]);
+  t.equal(ayoayo.isGameOver, true);
+  t.equal(ayoayo.winner, -1);
+  t.end();
+});
+
+tap.test('should throw an error if trying to play an empty cell', function (t) {
+  const ayoayo = new Ayoayo();
+  ayoayo.play(4);
+  t.throws(() => ayoayo.play(3));
+  t.end();
 });
